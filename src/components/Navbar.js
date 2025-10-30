057875d6c945ab84789a5f9904ca4daf4a7b25f5
DalipTandon <daliptandon8@gmail.com>
2025-10-31 00:03:37 +0530

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,18 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-md h-[15vh] w-full flex items-center">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="bg-white h-[15vh] w-full flex justify-center ">
+      <div className=" px-4 w-full py-4 lg:px-8 md:w-[80vw] md:m-5 ">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
-            <img className="h-8 w-auto" src="/assets/logo.png" alt="Crypto Wallet Logo" />
+            <h3 className="text-4xl font-bold text-blue-600">DLock</h3>
           </div>
-          <div className="hidden md:ml-6 md:flex md:space-x-8">
+          <div className="py-4 md:ml-6 md:flex md:space-x-8">
             <a href="#" className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium">Home</a>
-            <a href="#" className="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium">Features</a>     
-            <a href="#" className="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium">Pricing</a>
-            <a href="#" className="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium">Contact</a>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
 
