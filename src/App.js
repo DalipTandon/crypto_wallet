1830adbfcec1fdce8606aaa6c57278b217155eae
DalipTandon <daliptandon8@gmail.com>
2025-10-31 22:55:00 +0530

    add react router dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import { BrowserRouter, RouterProvider, Routes } from 'react-router';
+import { Route } from 'react-router';
+import WalletGenerator from './components/WalletGenerator';
 
 function App() {
   return (
-    <div className='h-full  flex flex-col'>
-
-      <Navbar />
-      <Hero />
-
-      </div>
+    <BrowserRouter basename='/'>
+      <Routes>
+    <Route path='/' element={<Hero />} />
+    <Route path='/wallet' element={<WalletGenerator/>} />
+      </Routes>      
+    </BrowserRouter>
+  
   );
 }
 
